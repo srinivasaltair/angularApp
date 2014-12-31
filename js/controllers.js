60cc31275a0f4031178acebb12a078b645849db7
SrinivasP <srnvs0708@gmail.com>
2014-12-31 18:04:54 +0530

    Added Sort feature for it..

--- js/controllers.js
+++ js/controllers.js
@@ -3,14 +3,20 @@ var phonecatApp = angular.module('phonecatApp',[]);
 phonecatApp.controller('PhoneListCtrl',function($scope){
 	$scope.phones = [
 		{'name':'Nexus 5',
-		'snippet':'Just got faster and smarter'},
+		'snippet':'Just got faster and smarter',
+		'new':'2'},
 		{'name':'Moto e',
-		'snippet':'smaller yet powerfull'},
+		'snippet':'smaller yet powerfull',
+		'new':'3'},
 		{'name':'iPhone 6',
-		'snippet':'same old classy with new looks'},
+		'snippet':'same old classy with new looks',
+		'new':'4'},
 		{'name':'Samsung',
-		'snippet':'no difference same old design'}
+		'snippet':'no difference same old design',
+		'new':'1'}
 	]
+	
+	$scope.orderProp = 'new';
 });
 
 phonecatApp.controller('Uname',function($scope){
